edb4750df6935b51d9b3d69f58388befa2a4098b
Errol Sayre <esayre@olemiss.edu>
2012-06-19 10:55:21 -0500

    Imports initial content

---
+++ global-git.php
@@ -0,0 +1,34 @@
+#!/usr/bin/php
+<?php
+// define helpful constants
+if (!defined('LF'))	define('LF', "\n");
+
+// locate the current tree
+$dir = 	getcwd();
+echo 'Git repositories in ', $dir, LF;
+
+// determine if there are any arguments
+$arguments = array();
+if (count($argv) > 1) {
+	$arguments = array_slice($argv, 1);
+}
+if (count($arguments)) {
+	$arguments = implode(' ', $arguments);
+} else {
+	$arguments = 'status';
+}
+
+// locate git repositories in the current tree
+$response = `find . -name ".git" -type d`;
+$matches = explode(LF, $response);
+foreach ($matches as $match) {
+	if ($match) {
+		// locate the parent directory
+		$match = pathinfo($match, PATHINFO_DIRNAME);
+		echo $match, LF;
+		// remove the leading . placed by find
+		$match = $dir.ltrim($match, '.');
+		chdir($match);
+		echo `git $arguments`, LF;
+	}
+}
